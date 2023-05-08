171164e88c2735be367f798728c84444b3e7fee9
why <2594258643@qq.com>
2023-05-08 08:52:08 +0800

    提交初始化版本

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: zhouchunchun 316581172@qq.com
+ * @Date: 2023-05-04 16:19:07
+ * @LastEditors: zhouchunchun 316581172@qq.com
+ * @LastEditTime: 2023-05-06 15:49:34
+ * @FilePath: \apid:\vscode\vue-project\demo\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div id="app">
+    <router-view  />
+  </div>
+</template>
+<script>
+
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    
+  },
+};
+</script>
+<style>
+html,
+body,
+#app {
+  background-color: rgb(227, 231, 231);
+  padding: 0;
+  margin: 0;
+}
+
+</style>
