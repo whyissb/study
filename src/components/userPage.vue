ea4109607a51f33f316e53be24ecc2ba37b5df21
why <2594258643@qq.com>
2023-05-10 14:53:25 +0800

    feat:aaa

--- src/components/userPage.vue
+++ src/components/userPage.vue
@@ -1,40 +1,24 @@
 <!--
  * @Author: zhouchunchun 316581172@qq.com
  * @Date: 2023-05-06 14:07:41
-<<<<<<< HEAD
  * @LastEditors: why 2594258643@qq.com
  * @LastEditTime: 2023-05-08 16:46:01
-=======
- * @LastEditors: zhouchunchun 316581172@qq.com
- * @LastEditTime: 2023-05-06 15:03:32
->>>>>>> 171164e88c2735be367f798728c84444b3e7fee9
  * @FilePath: \apid:\vscode\vue-project\demo\src\components\userPage.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
     <div>
-<<<<<<< HEAD
     111
-=======
-        2266
->>>>>>> 171164e88c2735be367f798728c84444b3e7fee9
     </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-
-=======
->>>>>>> 171164e88c2735be367f798728c84444b3e7fee9
 export default {
     name: 'userPage',
 
     data() {
         return {
-<<<<<<< HEAD
             name:1
-=======
->>>>>>> 171164e88c2735be367f798728c84444b3e7fee9
             
         };
     },
@@ -46,10 +30,7 @@ export default {
     methods: {
         
         
-<<<<<<< HEAD
         
-=======
->>>>>>> 171164e88c2735be367f798728c84444b3e7fee9
     },
 };
 </script>
