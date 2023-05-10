0cf4f74c302509eeff298d180b2ad037a239a11e
why <2594258643@qq.com>
2023-05-10 15:20:53 +0800

    feat

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,5 @@
-<!--
- * @Author: zhouchunchun 316581172@qq.com
- * @Date: 2023-05-04 16:19:08
- * @LastEditors: zhouchunchun 316581172@qq.com
- * @LastEditTime: 2023-05-06 15:14:29
- * @FilePath: \apid:\vscode\vue-project\demo\src\views\AboutView.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
-    <template>
-    <div>
+<template>
+  <div>
   <div>
     <!-- 为 ECharts 准备一个定义了宽高的 DOM -->
     <div id="main" style="width: 600px; height: 400px"></div>
